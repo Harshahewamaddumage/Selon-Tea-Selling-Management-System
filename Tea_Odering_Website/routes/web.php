225b3ff1c809fc1ce008cd5b1d2ff97d52eef5c7
Harshahewamaddumage <harsha99114@gmail.com>
2024-12-21 23:15:37 +0530

    Update

--- Tea_Odering_Website/routes/web.php
+++ Tea_Odering_Website/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\homecontroller;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/home',[homecontroller::class,'home']);
